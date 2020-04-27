9531cee5a155b33e41b143604cc1b833aa890555
2021agreene <2021agreene@dwight.edu>
2020-04-27 10:22:28 -0400

    Final

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,11 @@ from flask_pymongo import PyMongo
 from flask import Flask
 from bson.objectid import ObjectId
 from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 
-# app = Flask(__name__)
-app.config.from_pyfile('config.cfg')
-
-mail = Mail(app)
+#s = URLSafeTimedSerializer('Thisisasecret')
+#app.config.from_pyfile('config.cfg')
+#mail = Mail(app)
 app.config['MONGO_DBNAME'] = 'IA'
 
 if __name__ == '__main__':
@@ -27,21 +27,15 @@ login = True
 def index():
     user_info = dict(request.form)
     return render_template('index.html')
-
-@app.route('/practice', methods = ["get", "post"])
-def practice():
-    if request.method == "get":
-        return '<form action ="/" method="post"><input name="email"><input type = "submit"></form>'
-    return 'the email you entered is {}'.format(request.form['email'])
 @app.route('/add', methods = ["get", "post"])
 def add():
     user_info = dict(request.form)
     event_name = user_info["name"]
     event_date = user_info["date"]
     event_category = user_info["category"]
-    event_time = user_info["description"]
+    event_description = user_info["description"]
     collection = mongo.db.events
-    collection.insert({"name": event_name, "date": event_date, "category": event_category, "time": event_time})
+    collection.insert({"name": event_name, "date": event_date, "category": event_category, "description": event_description})
     return redirect('/homepage')
 @app.route('/signup1', methods = ["get", "post"])
 def signup():
@@ -51,6 +45,12 @@ def signup():
     name = user_info["name"]
     collection = mongo.db.information
     query = list(collection.find({"email": email}))
+    #token = s.dumps(email, salt ='email-confirm')
+    #msg = Message('Confirm Email', sender='dwightcaswebsite@gmail.com', recipients = [email])
+    #link = url_for('confirm_email', token = token, _external=True)
+    #msg.body = 'Your link is {}'.format(link)
+    #mail.send(msg)
+    #print(token)
     if len(query) > 0:
         return render_template('error.html', error = "Account was already created!", link = "/signup")
     elif len(query) == 0:
@@ -59,11 +59,16 @@ def signup():
             return render_template('error.html', error = "Account was succesfully created!", link = "/signup")
         else:
             return render_template('error.html', error = "Email is not apart of dwight.", link = "/signup")
-
+@app.route('/confirmemail/<token>')
+def confirm_email(token):
+    try:
+        email = s.loads(token, salt='email-confirm', max_age=20)
+    except SignatureExpired:
+        return "the token is expired"
+    return "the token works"
 @app.route('/signup')
 def test():
     return render_template('signup.html')
-
 @app.route('/login', methods = ["get", "post"])
 def login():
     global login
@@ -93,7 +98,6 @@ def homepage():
         return render_template('homepage.html', events = events, test = test)
     else:
         return redirect('/index')
-
 @app.route('/filter', methods = ["get", "post"])
 def function():
     collection = mongo.db.events
@@ -111,3 +115,32 @@ def delete():
     delete = user_input["_id"]
     collection.delete_one({"_id": ObjectId(delete)})
     return redirect('/homepage')
+@app.route('/details', methods = ["get", "post"])
+def details():
+    collection = mongo.db.events
+    user_input = dict(request.form)
+    name = user_input["_id"]
+    list = collection.find({"_id": ObjectId(name)})
+    return render_template('details.html', events = list)
+@app.route('/register', methods = ["get", "post"])
+def register():
+    return render_template('register.html')
+@app.route('/registeradd', methods = ["get", "post"])
+def registeradd():
+    user_input = dict(request.form)
+    collection = mongo.db.attendies
+    event_name = user_input["ename"]
+    your_name = user_input["uname"]
+    event_email = user_input["email"]
+    list = collection.insert({"name": event_name, "yourname": your_name, "email": event_email})
+    return redirect('/homepage')
+@app.route('/attending')
+def attending():
+    return render_template('viewform.html')
+@app.route('/viewattendies', methods = ["get", "post"])
+def attendies():
+    user_input = dict(request.form)
+    collection = mongo.db.attendies
+    event_name = user_input["name"]
+    list = collection.find({"name": event_name})
+    return render_template('viewattendies.html', events = list)
